a900ee6e34fa264937e066e1621fb3be1328b145
Michael James P Roy <Shigenoroy@gmail.com>
2025-02-16 20:01:45 +0800

    adding second verse to the lyrics

--- main.java
+++ main.java
@@ -1,8 +1,16 @@
-public class main{
+public class main {
     public static void main(String[] args) {
         System.out.println("Yeah, I'm gonna take my horse to the old town road\r\n" + //
-                        "I'm gonna ride 'til I can't no more\r\n" + //
-                        "I'm gonna take my horse to the old town road\r\n" + //
-                        "I'm gonna ride (Kio, Kio) 'til I can't no more");
+                "I'm gonna ride 'til I can't no more\r\n" + //
+                "I'm gonna take my horse to the old town road\r\n" + //
+                "I'm gonna ride (Kio, Kio) 'til I can't no more");
+        System.out.println("I got the horses in the back\r\n" + //
+                "Horse tack is attached\r\n" + //
+                "Hat is matte black\r\n" + //
+                "Got the boots that's black to match\r\n" + //
+                "Riding on a horse, ha\r\n" + //
+                "You can whip your Porsche\r\n" + //
+                "I been in the valley\r\n" + //
+                "You ain't been up off the porch now");
     }
 }
