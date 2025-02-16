1ce2b5deaf619f785a08089483bf29a81e7c3d68
Michael James P Roy <Shigenoroy@gmail.com>
2025-02-16 19:53:22 +0800

    The first commit - commiting all files to repository

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main{
+    public static void main(String[] args) {
+        System.out.println("Yeah, I'm gonna take my horse to the old town road\r\n" + //
+                        "I'm gonna ride 'til I can't no more\r\n" + //
+                        "I'm gonna take my horse to the old town road\r\n" + //
+                        "I'm gonna ride (Kio, Kio) 'til I can't no more");
+    }
+}
